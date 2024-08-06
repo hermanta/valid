2eaec3fde9ed6354d92c4a75e72c60f7e871c1a6
ihsangan <me@isan.eu.org>
2024-08-06 16:30:22 +0800

    update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,25 +5,24 @@ export default async function serveResult(request: Request): Promise<Response> {
   const dc = getUrl(request).searchParams.get('decode');
   let code = 200;
   let result: Result = await callAPI(request);
-  if (result.name !== undefined) {
+  if (result.name) {
     result.name = result.name.replace(/\u002B/g, '%20');
-    if (dc === null || dc === 'true') {
+    if (dc === null || dc === 'true' || dc !== 'false') {
       result.name = decodeURIComponent(result.name);
     }
   }
   if (result.message === 'Bad request') {
     code = 400;
-    delete result.name;
   }
   if (result.message === 'Not found') {
     code = 404;
-    delete result.name;
   }
   const response = new Response(JSON.stringify(result), {
     status: code,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, HEAD',
+      'Access-Control-Expose-Headers': 'X-Response-Time',
       'Cache-Control': 'max-age=40, proxy-revalidate, immutable',
       'Content-Type': 'application/json; charset=utf-8',
     }
